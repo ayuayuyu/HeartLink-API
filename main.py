1d4aa8af313c22184a53ee97b2425da12c1afc6c
ayu <umaminokiami@gmail.com>
2024-09-22 03:12:08 +0900

    update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from typing import List
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
-from src.WsManager import WsManager
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+
+from src.WsManager import WsManager
+from src.filter import filter
+from src.models import Datas
 
 app = FastAPI()
 manager = WsManager()
@@ -15,33 +17,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-class Datas(BaseModel):
-    heartRate: str
     
-class Reset(BaseModel):
-    value: str
-    
-    
-def max():
-    try:
-        if float(manager.get_heartMax()) < float(manager.get_heart()):
-            manager.set_heartMax(manager.get_heart())
-    except ValueError:
-        if int(manager.get_heartMax()) < int(manager.get_heart()):
-            manager.set_heartMax(manager.get_heart())
-        
 
 @app.get("/")
 async def get():
     return HTMLResponse("Hello World!")
 
-# @app.post("/reset")
-# async def reset_endpoint(reset: Reset):
-#     manager.set_heart(reset.value)
-#     manager.set_heartMax(reset.value)
-#     await manager.broadcast(manager.get_heart(),'12345')
-#     print(f"heartMax: {manager.get_heartMax()} , heartRate: {manager.get_heart()}")
     
 @app.post("/max")
 async def max_endpoint():
@@ -50,13 +31,11 @@ async def max_endpoint():
 @app.post("/data")
 async def create_data(data: Datas):
     print(f"心拍数: {data.heartRate}")
-    manager.set_heart(data.heartRate)  # 心拍数をセットする
-    max()
-    print(f"heartMax: {manager.get_heartMax()}")
-    # WebSocketを使ってクライアントに心拍数をブロードキャスト
-    await manager.broadcast(manager.get_heart(),'12345')
-        
-    return manager.get_heart()
+    # 心拍数をセットする(通常、最大値、最小値)
+    filter.allSet(data.heartRate)
+    # WebSocketに心拍数を送る
+    await manager.broadcast(filter.get_heart,'12345')
+    return filter.get_heart()
 
 
 @app.websocket("/ws/{room_id}")
@@ -67,9 +46,8 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str):
             # クライアントからのメッセージ受信
             data = await websocket.receive_text()
             print(f"送信する心拍数: {data}")
-            # リセットボタンを押さずともwebsocket.sendで送られてきた値を０にしてそれを入れればリセットがいらなくなる
-            manager.set_heart(data)
-            manager.set_heartMax(data)
+            # 通常、最大値、最小値のすべての値をリセットする
+            filter.reSet(data)
             # ルーム内の全クライアントにブロードキャスト
             await manager.broadcast(data, room_id)
     except WebSocketDisconnect:
