3f781458bb5a78eb8884c9c707720f85e61e0f9e
ayu <umaminokiami@gmail.com>
2024-09-21 23:30:05 +0900

    updatte

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ async def get():
 async def reset_endpoint(reset: Reset):
     manager.set_heart(reset.value)
     manager.set_heartMax(reset.value)
+    await manager.broadcast(manager.get_heart(),'12345')
     print(f"heartMax: {manager.get_heartMax()} , heartRate: {manager.get_heart()}")
     
 @app.post("/max")
@@ -39,33 +40,25 @@ async def max_endpoint():
 @app.post("/data")
 async def create_data(data: Datas):
     print(f"心拍数: {data.heartRate}")
-    manager.set_heart(data.heartRate)  # 心拍数をマネージャーにセット
+    manager.set_heart(data.heartRate)  # 心拍数をセットする
     if manager.get_heartMax() < manager.get_heart():
         manager.set_heartMax(manager.get_heart())
         print(f"heartMax: {manager.get_heartMax()}")
     # WebSocketを使ってクライアントに心拍数をブロードキャスト
-    try:
-        await manager.broadcast(manager.get_heart(), '12345')
-    except Exception as e:
-        print(f"Error broadcasting message: {e}")
-    return {"status": "Message sent via WebSocket"}
+    await manager.broadcast(manager.get_heart(),'12345')
+        
+    return manager.get_heart()
 
 
 @app.websocket("/ws/{room_id}")
 async def websocket_endpoint(websocket: WebSocket, room_id: str):
-    await websocket.accept()
     await manager.connect(websocket, room_id)
     try:
         while True:
             # クライアントからのメッセージ受信
-            await websocket.receive_text()
-            print(f"送信する心拍数: {manager.get_heart()}")
+            data = await websocket.receive_text()
+            print(f"送信する心拍数: {data}")
             # ルーム内の全クライアントにブロードキャスト
-            await manager.broadcast(manager.get_heart(), room_id)
+            await manager.broadcast(data, room_id)
     except WebSocketDisconnect:
         manager.disconnect(websocket, room_id)
-    finally:
-        try:
-            await websocket.close()
-        except Exception as e:
-            print(f"Error during closing handshake: {e}")
--- src/WsManager.py
+++ src/WsManager.py
@@ -9,24 +9,23 @@ class WsManager:
         self.active_connections: Dict[str, List[WebSocket]] = {}
 
     async def connect(self, websocket: WebSocket, room_id: str):
+        await websocket.accept()
         if room_id not in self.active_connections:
             self.active_connections[room_id] = []
         self.active_connections[room_id].append(websocket)
 
     def disconnect(self, websocket: WebSocket, room_id: str):
+        websocket.close()
         self.active_connections[room_id].remove(websocket)
         if not self.active_connections[room_id]:  # もし部屋に接続が無ければ部屋を削除
             del self.active_connections[room_id]
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
 
     async def broadcast(self, message: str, room_id: str):
         for connection in self.active_connections.get(room_id, []):
             try:
                 await connection.send_text(message)
-            except Exception as e:
-                print(f"Error sending message: {e}")
+            except:
+                self.active_connections.remove(connection)
 
     def set_heart(self, heart_value: str):
         self.heart = heart_value
