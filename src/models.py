f9d3e9d3ec20d76f7940d792332899bd523eab33
ayu <umaminokiami@gmail.com>
2024-10-07 00:28:03 +0900

    fix

---
+++ src/models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class Datas(BaseModel):
+    heartRate: str
+    id: str
+    
+class Reset(BaseModel):
+    value: str
+    
+class Device(BaseModel):
+    id: str
+    
+class Status(BaseModel):
+    status: str
